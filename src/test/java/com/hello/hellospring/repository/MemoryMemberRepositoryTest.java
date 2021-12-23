caaa5ea7e3543fdf8f257faedc012e18abe73605
dohyeon-han <limited.hyeon@gmail.com>
2021-12-23 23:30:25 +0900

    unit test

---
+++ src/test/java/com/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.hello.hellospring.repository;
+
+import com.hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    //하나의 테스트가 끝날 때마다 테스트한 것을 초기화 해준다.
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    public void findById(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member ret = repository.findById(member.getId()).get();
+        //System.out.println(ret == member);
+        //Assertions.assertEquals(member, ret);
+        assertThat(member).isEqualTo(ret);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member ret = repository.findByName("spring2").get();
+
+        assertThat(ret).isEqualTo(member2);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> list = repository.findAll();
+
+        assertThat(list.size()).isEqualTo(2);
+    }
+
+}
---
+++ src/test/java/com/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,10 @@
+package com.hello.hellospring.service;
+
+import com.hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+
+
+}
